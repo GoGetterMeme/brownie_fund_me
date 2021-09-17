70c044b0024cac20d00998847bbc7f7f7867ff0c
YetiBridge <yetiswap@protonmail.com>
2021-09-17 15:59:44 -0400

    updated

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,24 +1,27 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-def deploy_simple_storage():
-    # account = accounts.add(config["wallets"]["from_key"])
-    account = get_account()
-    simple_storage = SimpleStorage.deploy({"from": account})
-    stored_value = simple_storage.retrieve()
-    print(stored_value)
-    transaction = simple_storage.store(15, {"from": account})
-    transaction.wait(1)
-    updated_stored_value = simple_storage.retrieve()
-    print(updated_stored_value)
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def get_account():
-    if network.show_active() == "development":
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
 
 
-def main():
-    deploy_simple_storage()
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(8, 200000000000, {"from": get_account()})
+    print("Mocks Deployed!")
